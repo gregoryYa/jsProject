86abd8c96b0c43d1ebe5c418a31b6985997b9ace
Gregory <grisha.yaroshenko.gmail.com>
2019-09-21 17:00:28 +0300

    This is first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+// console.log (4/0);
+// alert("pidorasina!");
+// let answer = confirm("are you here?");
+// console.log(answer);
+
+// let answer = prompt("Are you 18 years old?", "");
+// console.log(typeof(answer));
+
+let isOpened = true,
+    isClosed = false;
+console.log(isOpened && isClosed);
+

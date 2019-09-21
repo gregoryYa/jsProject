8450db6270eb2a0866eda8bbab958f2d84add0db
Gregory <grisha.yaroshenko.gmail.com>
2019-09-21 18:18:17 +0300

    Commit for the first task

--- script.js
+++ script.js
@@ -9,7 +9,33 @@
 // let answer = prompt("Are you 18 years old?", "");
 // console.log(typeof(answer));
 
-let isOpened = true,
-    isClosed = false;
-console.log(isOpened && isClosed);
+// let isOpened = true,
+//     isClosed = false;
+// console.log(isOpened && isClosed);
+
+var money = prompt("What is your budget per month?");
+//console.log(money);
+
+var time = prompt("Put the date in format DD-MM-YY");
+//console.log
+var appData = {
+     budget: money,
+     timeData: time,
+     expenses: q1,
+     //optionalExpenses: 
+     //income: 
+     savings: false,
+
+} ;
+
+var q1 = prompt("What is a obligatory expenses for this month?");
+var q2 = prompt("How much will it cost?");
+var expenses = {
+    q1: q2,
+};
+
+alert("Your daily budget is " + (money / 30));
+
+console.log(appData.budget);
+
 

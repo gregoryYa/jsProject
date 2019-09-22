eb6096ead07bee50aba0db9b5e33c76636ab48f3
Gregory <grisha.yaroshenko.gmail.com>
2019-09-23 00:01:52 +0300

    SecondTaskResolved

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 'use strict';
 
-
-// console.log (4/0);
-// alert("pidorasina!");
-// let answer = confirm("are you here?");
-// console.log(answer);
-
-// let answer = prompt("Are you 18 years old?", "");
-// console.log(typeof(answer));
-
-// let isOpened = true,
-//     isClosed = false;
-// console.log(isOpened && isClosed);
-
-var money = prompt("What is your budget per month?", ''),
+var money = +prompt("What is your budget per month?", ''),
     time = prompt("Put the date in format DD-MM-YY", '');  
 
 var appData = {
@@ -25,17 +12,64 @@ var appData = {
      savings: false
 } ;
 
-var q1 = prompt("What is a obligatory expenses for this month?"),
-    q2 = prompt("How much will it cost?"),
-    q3 = prompt("What is a obligatory expenses for this month?"),
-    q4 = prompt("How much will it cost?");
+// FOR CYCLE
 
-appData.expenses.q1 = q2;
-appData.expenses.q3 = q4;
+// for (var i = 0; i < 2; i++) {
+//     var a = prompt("What is a obligatory expenses for this month?", ''),
+//         b = prompt("How much will it cost?", '');
+    
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(a)) != null && a != '' && b != '' 
+//     && a.length < 50  ) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//     }
+// };
 
-console.log(appData.expenses);
+// WHILE CYCLE
 
-alert("Your daily budget is " + (money / 30));
+// var i = 0;
+// while (i < 2) {
+//     var a = prompt("What is a obligatory expenses for this month?", ''),
+//         b = prompt("How much will it cost?", '');
+    
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(a)) != null && a != '' && b != '' && a.length < 50  ) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//     }
+//     i++;
+// }
+
+//DO CYCLE
+var i = 0;
+do {
+    var a = prompt("What is a obligatory expenses for this month?", ''),
+        b = prompt("How much will it cost?", '');
+    
+    if ( (typeof(a)) === 'integer' && (typeof(a)) != null && (typeof(a)) != null && a != '' && b != '' && a.length < 50  ) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        alert("YOU PUT WRONG NUMBERS. TRY AGAIN!");
+    }
+        i++;
+    }
+    while (i < 3);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Your daily budget is " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Low life quality") ;
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Medium life quality");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("High life quality"); 
+} else {
+    console.log("error error");
+}
 
 
 

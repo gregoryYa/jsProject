bfe76ba7637bfddb7b8eb5d632c37db2e52d7527
Gregory <grisha.yaroshenko.gmail.com>
2019-09-24 20:46:06 +0300

    FunctionsTask

---
+++ scriptFunctions13.js
@@ -0,0 +1,22 @@
+// function ShowFirstMessage(text) {
+//     alert(text);
+// }
+
+//ShowFirstMessage('Hello WORLD!');
+
+// function calc() {
+//     let c = +prompt("Put a");
+//     let d = +prompt("Put b");
+//     return alert("a + b= " + (c+d));
+    
+// }
+// calc();
+
+var calcNewJSES6 = (a,b) => {a + b}
+console.log(calcNewJSES6(4,5));
+
+let str = 'test';
+console.log(str.length);
+
+
+
